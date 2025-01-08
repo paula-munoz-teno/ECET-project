c89725c312106d6c1cf2f54b31b93ac3c036c2c3
Paula-Munoz-Teno <paulamtlaboral@gmail.com>
2025-01-08 20:28:08 +0100

    prototipo1

--- angular-hello-world/src/app/app-routing.module.ts
+++ angular-hello-world/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { PartnersComponent } from './pages/partners/partners.component';
+import { ReaserchComponent } from './pages/reaserch/reaserch.component';
+import { BlogComponent } from './pages/blog/blog.component';
 
 const routes: Routes = [
   {path: "", redirectTo: '/home', pathMatch: 'full' }, // Ruta por defecto
- {path:"home", component:HomeComponent}
+ {path:"home", component:HomeComponent},
+ {path:"about", component:AboutComponent},
+ {path:"research", component:ReaserchComponent},
+ {path:"contact", component:ContactComponent},
+ {path:"teamWork", component:PartnersComponent},
+ {path:"blog", component:BlogComponent}
+
+
+
 ];
 
 @NgModule({
@@ -12,3 +25,5 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+//EL path debe ser el mismo que el que se denomina el routerlink  
